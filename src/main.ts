de466e323f7cb7c45a3988e34b8da57dabf03d1b
quocdatk18 <quocdatlop109@gmail.com>
2025-08-03 22:53:29 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   });
   app.use(passport.initialize());
